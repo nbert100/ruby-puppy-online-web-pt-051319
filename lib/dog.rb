a7257b2757d330fa5a7bd6c02d193d50519c664d
Nia Bert <nbert14@gmail.com>
2019-06-01 23:49:36 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(dog)
+    @name = name
+    @@all << self
+  end
+  
+  def self.clear_all
+  end
+  
+  def self.all
+  end
+end
